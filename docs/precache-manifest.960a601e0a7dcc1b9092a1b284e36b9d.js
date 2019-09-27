336ee4f0ac825f227508bff03a03568846652eff
Christian Bjartli <git@c.bjart.li>
2019-09-28 01:37:56 +0200

    Add build

---
+++ docs/precache-manifest.960a601e0a7dcc1b9092a1b284e36b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b06c5ae2b61f5354a1b86d5d9cbc791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac08105bca02cb2f49e",
+    "url": "/static/css/2.189d53d3.chunk.css"
+  },
+  {
+    "revision": "a27c96ad3d3807503fb4",
+    "url": "/static/css/main.14a65df7.chunk.css"
+  },
+  {
+    "revision": "aac08105bca02cb2f49e",
+    "url": "/static/js/2.09556534.chunk.js"
+  },
+  {
+    "revision": "a27c96ad3d3807503fb4",
+    "url": "/static/js/main.e5f810c9.chunk.js"
+  },
+  {
+    "revision": "75ac6d833ef977621b75",
+    "url": "/static/js/runtime-main.3d181396.js"
+  }
+]);
